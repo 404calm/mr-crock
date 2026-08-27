4544f2061536554f79731704c91032882fca0d2c
Carlos LdM <carloslopezdemadariaga@gmail.com>
2026-08-27 13:26:53 +0200

    Fix mobile hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,6 @@
-console.log("script.js cargado correctamente");
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu_nav_items");
 
-// ========== MENÚ HAMBURGUESA ==========
-const botonMenu = document.querySelector(".menu-btn");  // ← Selector correcto
-const menuContenido = document.querySelector(".menuNavItems");
-
-if (botonMenu && menuContenido) {
-    botonMenu.addEventListener("click", () => {
-        menuContenido.classList.toggle("mostrar");
-    });
-}
-//LINK SABER +//(".textLink");
-const botones = document.querySelectorAll(".textLink");
-
-botones.forEach((boton) => {
-    boton.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const tarjeta = boton.closest(".cardItem");
-
-        if (!tarjeta) return;
-
-        const texto = tarjeta.querySelector(".textoOculto");
-
-        texto.classList.toggle("show");
-
-        if (texto.classList.contains("show")) {
-            boton.textContent = "OCULTAR";
-        } else {
-            boton.textContent = "SABER MÁS +";
-        }
-    });
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu_open");
 });
-
